842ea53e49b4d38d00243e8eb94bab53fb4165c1
ErneDainesi <edainesi@fi.uba.ar>
2022-09-11 16:36:51 -0300

    code refactor

--- src/main.rs
+++ src/main.rs
@@ -5,61 +5,77 @@ const FILE_NAME: &str = "./table.txt";
 const MINE: u8 = b'*';
 
 struct MineField<'a> {
-    field: Vec<&'a [u8]>
+    matrix: Vec<&'a [u8]>
 }
 
-struct Neighbour {
-    value: usize,
-    is_valid: bool
+struct Field {
+    neighbours: Vec<u8>
 }
 
-struct Neighbourhood {
-    get_neighbours: Neighbour,
-    left: Neighbour,
-    front: Neighbour,
-    right: Neighbour,
-    top_left: Neighbour,
-    top_right: Neighbour,
-    bottom_left: Neighbour,
-    bottom_right: Neighbour
-}
+impl Field {
+    pub fn new(matrix: &MineField, row: usize, column: usize) -> Self {
+        Self {
+            neighbours: Self::get_neighbours(matrix, row, column)
+        }
+    }
 
-impl Neighbourhood {
-    // pub fn new(matrix: Vec<Vec<&u8>>, row: usize, column: usize) -> Self {
-    //     // Self {
-    //     //     is_valid: (),
-    //     //     value: ()
-    //     // }
-    // }
-
-    // fn check_if_row_is_valid() -> bool {
-    //     if 
-    // }
-
-    fn get_neighbours(matrix: Vec<Vec<&u8>>, row: usize, column: usize) -> [usize; 4] {
-        let left_idx = safe_decrement(column, matrix[row].len());
-        let top_idx = safe_decrement(row, matrix[row].len());
+    fn get_neighbours(mine_field: &MineField, row: usize, column: usize) -> Vec<u8> {
+        let left_idx = safe_decrement(column, mine_field.matrix[row].len());
+        let top_idx = safe_decrement(row, mine_field.matrix[row].len());
         let bottom_idx = row + 1;
         let right_idx = column + 1;
-        let idxs = [top_idx, right_idx, bottom_idx, left_idx];
-        idxs
+        let mut neighbours: Vec<u8> = vec![];
+        let row_length = mine_field.matrix[row].len();
+        if right_idx < row_length {
+            neighbours.push(mine_field.matrix[row][right_idx]);
+        }
+        if left_idx < row_length {
+            neighbours.push(mine_field.matrix[row][left_idx]);
+        }
+        if top_idx < row_length && right_idx < row_length {
+            neighbours.push(mine_field.matrix[top_idx][right_idx]);
+        }
+        if top_idx < row_length && left_idx < row_length {
+            neighbours.push(mine_field.matrix[top_idx][left_idx]);
+        }
+        if bottom_idx < mine_field.len() && left_idx < row_length {
+            neighbours.push(mine_field.matrix[bottom_idx][left_idx]);
+        }
+        if top_idx < row_length {
+            neighbours.push(mine_field.matrix[top_idx][column]);
+        }
+        if bottom_idx < mine_field.len() && right_idx < row_length {
+            neighbours.push(mine_field.matrix[bottom_idx][right_idx]);
+        }
+        if bottom_idx < mine_field.len() {
+            neighbours.push(mine_field.matrix[bottom_idx][column]);
+        }
+        neighbours
+    }
+
+    fn sweep_mines(&self, count: &mut i32) {
+        for field in self.neighbours.iter() {
+            if is_mine(*field) {
+                *count += 1
+            }
+        }
     }
 }
 
 impl<'a> MineField<'a> {
     pub fn new() -> Self {
-        let field: Vec<&[u8]> = vec![];
+        let matrix: Vec<&[u8]> = vec![];
         Self {
-            field
+           matrix
         }
     }
 
     pub fn push(&mut self, row: &'a [u8]) {
-        self.field.push(row);
+        self.matrix.push(row);
     }
 
     pub fn len(&self) -> usize {
-        self.field.len()
+        self.matrix.len()
     }
 }
 
@@ -83,89 +99,27 @@ fn safe_decrement(n: usize, default_value: usize) -> usize {
 }
 
 fn parse_lines(buf: Vec<String>) {
-    let mut matrix = MineField::new();
-    let mut output: Vec<String> = vec![];
+    let mut mine_field = MineField::new();
     for line in &buf {
-        matrix.push(line.as_bytes())
+        mine_field.push(line.as_bytes())
     }
     let mut res = "".to_string();
-    for i in 0..matrix.len() {
-        for j in 0..matrix.field[i].len() {
+    for i in 0..mine_field.len() {
+        for j in 0..mine_field.matrix[i].len() {
             let mut counter = 0;
-            if is_mine(matrix.field[i][j]) {
+            if is_mine(mine_field.matrix[i][j]) {
                 res += "*";
                 continue;
             }
-            let left_idx = safe_decrement(j, matrix.field[i].len());
-            let top_idx = safe_decrement(i, matrix.field[i].len());
-            let bottom_idx = i + 1;
-            let right_idx = j + 1;
-
-            if right_idx < matrix.field[i].len() {
-                let right = matrix.field[i][right_idx];
-                if is_mine(right) {
-                    counter += 1;
-                }
-            }
-
-            if left_idx != matrix.field[i].len() {
-                let left = matrix.field[i][left_idx];
-                if is_mine(left) {
-                    counter += 1;
-                }
-            }
-
-            if top_idx != matrix.field[i].len() && right_idx < matrix.field[i].len() {
-                let top_right = matrix.field[top_idx][right_idx];
-                if is_mine(top_right) {
-                    counter += 1;
-                }
-            }
-
-            if top_idx != matrix.field[i].len() && left_idx != matrix.field[i].len() {
-                let top_left = matrix.field[top_idx][left_idx];
-                if is_mine(top_left) {
-                    counter += 1;
-                }
-            }
-
-            if bottom_idx < matrix.len() && left_idx != matrix.field[i].len() {
-                let bottom_left = matrix.field[bottom_idx][left_idx];
-                if is_mine(bottom_left) {
-                    counter += 1;
-                }
-            }
-
-            if top_idx < matrix.field[i].len() {
-                let front = matrix.field[top_idx][j];
-                if is_mine(front) {
-                    counter += 1;
-                }
-            }
-
-            if bottom_idx < matrix.len() && right_idx < matrix.field[i].len() {
-                let bottom_right = matrix.field[bottom_idx][right_idx];
-                if is_mine(bottom_right) {
-                    counter += 1;
-                }
-            }
-
-            if bottom_idx < matrix.len() {
-                let back = matrix.field[bottom_idx][j];           
-                if is_mine(back) {
-                    counter += 1;
-                }
-            }
-
+            let field: Field = Field::new(&mine_field, i, j);
+            field.sweep_mines(&mut counter);
             if counter < 1 {
                 res += &".".to_string();
             } else {
                 res += &counter.to_string();
             }
         }
-
         println!("{:?}", res);
-        output.push(res);
         res = "".to_string();
     }
 }
