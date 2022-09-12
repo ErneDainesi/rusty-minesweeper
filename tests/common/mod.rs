1b040e2124f4d30b90711e40fc21f3edb2e22d00
ErneDainesi <edainesi@fi.uba.ar>
2022-09-12 11:28:25 -0300

    add setup function to integration tests

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,3 +1,19 @@
-pub fn setup() {
-    // code
+use std::fs::File;
+use std::io::prelude::*;
+
+pub fn setup() -> File {
+    match create_file() {
+        Ok(value) => {
+            value
+        },
+        Err(error) => {
+            panic!("[FILE CREATION ERROR]: {}", error);
+        }
+    }
+}
+
+fn create_file() -> std::io::Result<File> {
+    let mut file = File::create("test.txt")?;
+    file.write_all(b".*.*.\n..*..\n..*..\n.....")?;
+    Ok(file)
 }
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -2,5 +2,6 @@ mod common;
 
 #[test]
 fn first_test() {
+    crate::common::setup();
     assert_eq!(2 + 2, 4);
 }
